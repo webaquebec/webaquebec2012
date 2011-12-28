1bd1a958a61bcdd34f940658a23551b80bb03fab
Raphael Riel <raphael.riel@libeo.com>
2011-12-28 12:49:47 -0500

    Added database handling.

--- app/shared/conf.example.php
+++ app/shared/conf.example.php
@@ -1,9 +1,13 @@
 <?php
 
 $config = array(
-    'name' => 'raph',
+    'name' => 'identifiy_name',
     'db' => array(
-
+        'host' => 'host.domain',
+        'port' => '3306',
+        'user' => 'user',
+        'password' => 'pwd',
+        'database' => 'dbname',
     ),
     'cache' => TRUE,
 );
