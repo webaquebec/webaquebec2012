60f43eaa80cd09ec7de3162ef20c46697aca2542
Raphael Riel <raphael.riel@libeo.com>
2011-12-28 16:19:06 -0500

    Starred and image fields.

--- app/frontend/structure.sql
+++ app/frontend/structure.sql
@@ -1,8 +1,10 @@
 CREATE TABLE presentation (
     uid int(11) NOT NULL AUTO_INCREMENT,
     ordering int(11),
+    starred boolean DEFAULT 0,
     presenter_name_first varchar(255) NOT NULL,
     presenter_name_last varchar(255) NOT NULL,
+    presenter_image varchar(255) NOT NULL,
     presenter_resume text NOT NULL,
     conference_name varchar(255) NOT NULL,
     conference_resume text NOT NULL,
--- app/frontend/test_data.sql
+++ app/frontend/test_data.sql
@@ -1,24 +1,26 @@
 INSERT INTO `presentation` 
-(`uid`, 
-`ordering`, 
-`presenter_name_first`, 
-`presenter_name_last`, 
-`presenter_resume`, 
-`conference_name`, 
-`conference_resume`, 
-`conference_goals`, 
-`tags`, 
-`website`, 
-`twitter_handle`) 
+(`ordering`,
+`starred`,
+`presenter_name_first`,
+`presenter_name_last`,
+`presenter_image`,
+`presenter_resume`,
+`conference_name`,
+`conference_resume`,
+`conference_goals`,
+`tags`,
+`website`,
+`twitter_handle`)
 
 
 VALUES 
 
 (
 NULL, 
-NULL, 
-'Madame', 
-'ABC', 
+TRUE,
+'Madame',
+'ABC',
+'isabelle-grenier.jpg',
 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper posuere ligula in elementum. Cras ornare odio vitae arcu tincidunt tempus. Donec eget elementum dui. In hac habitasse platea dictumst. Nam convallis urna non est tempor auctor. Maecenas congue tristique lorem, ut bibendum leo euismod vitae. Etiam rhoncus consectetur vulputate. Aliquam mattis fermentum aliquet. Aenean tristique, augue vitae fermentum semper, justo elit ornare nisi, pretium porttitor nisi felis a elit. Vivamus id mi arcu, sed volutpat dui.
 
 Duis pharetra laoreet velit non ultrices. Etiam tempus tellus in tellus eleifend blandit. Cras id lacus purus. Praesent fermentum ante nec arcu adipiscing ac rutrum nisi pellentesque. Nullam vitae arcu a ante fermentum laoreet. Nulla est urna, condimentum vel rhoncus non, ornare sit amet lorem. Nam fringilla scelerisque massa sed pulvinar. Morbi convallis quam quis arcu dictum ac elementum sapien ultricies. Nam eleifend dolor vitae nibh porta bibendum. Duis non erat sapien. Morbi tincidunt turpis vitae nisl semper sodales. Nulla felis dolor, tempor eget eleifend vel, ultricies ut turpis.', 
@@ -46,9 +48,10 @@ Duis pharetra laoreet velit non ultrices. Etiam tempus tellus in tellus eleifend
 
 (
 NULL, 
-NULL, 
-'Madame', 
-'ABC', 
+FALSE,
+'Madame',
+'ABC',
+'remi-prevost.jpg',
 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper posuere ligula in elementum. Cras ornare odio vitae arcu tincidunt tempus. Donec eget elementum dui. In hac habitasse platea dictumst. Nam convallis urna non est tempor auctor. Maecenas congue tristique lorem, ut bibendum leo euismod vitae. Etiam rhoncus consectetur vulputate. Aliquam mattis fermentum aliquet. Aenean tristique, augue vitae fermentum semper, justo elit ornare nisi, pretium porttitor nisi felis a elit. Vivamus id mi arcu, sed volutpat dui.
 
 Duis pharetra laoreet velit non ultrices. Etiam tempus tellus in tellus eleifend blandit. Cras id lacus purus. Praesent fermentum ante nec arcu adipiscing ac rutrum nisi pellentesque. Nullam vitae arcu a ante fermentum laoreet. Nulla est urna, condimentum vel rhoncus non, ornare sit amet lorem. Nam fringilla scelerisque massa sed pulvinar. Morbi convallis quam quis arcu dictum ac elementum sapien ultricies. Nam eleifend dolor vitae nibh porta bibendum. Duis non erat sapien. Morbi tincidunt turpis vitae nisl semper sodales. Nulla felis dolor, tempor eget eleifend vel, ultricies ut turpis.', 
@@ -73,9 +76,10 @@ Duis pharetra laoreet velit non ultrices. Etiam tempus tellus in tellus eleifend
 
 (
 NULL, 
-NULL, 
-'Monsieur', 
-'XYZ', 
+TRUE,
+'Monsieur',
+'XYZ',
+'kim-auclair.jpg',
 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed semper posuere ligula in elementum. Cras ornare odio vitae arcu tincidunt tempus. Donec eget elementum dui. In hac habitasse platea dictumst. Nam convallis urna non est tempor auctor. Maecenas congue tristique lorem, ut bibendum leo euismod vitae. Etiam rhoncus consectetur vulputate. Aliquam mattis fermentum aliquet. Aenean tristique, augue vitae fermentum semper, justo elit ornare nisi, pretium porttitor nisi felis a elit. Vivamus id mi arcu, sed volutpat dui.
 
 Duis pharetra laoreet velit non ultrices. Etiam tempus tellus in tellus eleifend blandit. Cras id lacus purus. Praesent fermentum ante nec arcu adipiscing ac rutrum nisi pellentesque. Nullam vitae arcu a ante fermentum laoreet. Nulla est urna, condimentum vel rhoncus non, ornare sit amet lorem. Nam fringilla scelerisque massa sed pulvinar. Morbi convallis quam quis arcu dictum ac elementum sapien ultricies. Nam eleifend dolor vitae nibh porta bibendum. Duis non erat sapien. Morbi tincidunt turpis vitae nisl semper sodales. Nulla felis dolor, tempor eget eleifend vel, ultricies ut turpis.', 
