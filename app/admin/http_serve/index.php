898ea98e245ba5e8c9c63728676e27b1552ff30e
Raphael Riel <raphael.riel@gmail.com>
2012-01-05 17:40:03 -0500

    Basic CRUD. Missing a lot of polish.

--- app/admin/http_serve/index.php
+++ app/admin/http_serve/index.php
@@ -11,8 +11,67 @@ bootstrap(PATH_ADMIN."/templates/");
 
 $app->get('/', function () use ($app) {
     /** @var Slim $app */
-    $app->render('index.html');
+    $app->render('index.twig');
 })->name(AdminRoutes::INDEX);
+
+$app->get('/programmation/', function () use ($app) {
+    /** @var Slim $app */
+    $app->render('programmation/list.twig', array('list'=>Presentation::getAll()));
+})->name(AdminRoutes::PROGRAMMATION_LIST);
+
+
+$app->get('/programmation/edit/:id', function ($id) use ($app) {
+    /** @var Slim $app */
+    $app->render('programmation/new_or_edit.twig',
+        array(
+            'mode'=>'edit',
+            'item'=>Presentation::getById($id),
+        )
+    );
+})->name(AdminRoutes::PROGRAMMATION_EDIT);
+
+$app->get('/programmation/new/', function () use ($app) {
+    /** @var Slim $app */
+    $app->render('programmation/new_or_edit.twig',
+        array(
+            'mode'=>'new',
+            'item' => Presentation::create()
+        )
+    );
+})->name(AdminRoutes::PROGRAMMATION_NEW);
+
+$app->post('/programmation/create', function () use ($app) {
+    /** @var Slim $app */
+    $p = Presentation::create();
+    foreach($_POST as $k=>$v){
+        $p->set($k, $v);
+    }
+    $p->save();
+    $id = $p->get('id');
+
+    $app->flash('programmation', 'Créé!');
+    $app->redirect($app->urlFor(AdminRoutes::PROGRAMMATION_EDIT, array('id'=>$id)));
+})->name(AdminRoutes::PROGRAMMATION_CREATE);
+
+$app->post('/programmation/update/:id', function ($id) use ($app) {
+    /** @var Slim $app */
+    $p = Presentation::getById($id);
+    foreach($_POST as $k=>$v){
+        $p->set($k, $v);
+    }
+    $p->save();
+    $app->flash('programmation', 'Sauvegardé!');
+    $app->redirect($app->urlFor(AdminRoutes::PROGRAMMATION_EDIT, array('id'=>$id)));
+})->name(AdminRoutes::PROGRAMMATION_UPDATE);
+
+$app->get('/programmation/delete/:id', function ($id) use ($app) {
+    /** @var Slim $app */
+    $p = Presentation::getById($id);
+    $p->delete();
+
+    $app->flash('programmation', 'Supprimé!');
+    $app->redirect($app->urlFor(AdminRoutes::PROGRAMMATION_LIST));
+})->name(AdminRoutes::PROGRAMMATION_DELETE);
 
 $app->run();
 
--- app/shared/classes/Presentation.php
+++ app/shared/classes/Presentation.php
@@ -14,6 +14,13 @@ class Presentation extends Model{
     public static function getById($id){
         return Model::factory(__CLASS__)->find_one($id);
     }
+    /**
+     * @static
+     * @return Presentation
+     */
+    public static function create(){
+        return Model::factory(__CLASS__)->create();
+    }
 
     public function getTitleSlug(){
         return Helpers::generateSlug($this->get('conference_name'));
