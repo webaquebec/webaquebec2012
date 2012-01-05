bf4efd0bce5776305c10cb16adb21a8c8df45a32
Raphael Riel <raphael.riel@gmail.com>
2012-01-05 18:01:43 -0500

    Segment.

--- app/admin/http_serve/index.php
+++ app/admin/http_serve/index.php
@@ -20,58 +20,7 @@ $app->get('/programmation/', function () use ($app) {
 })->name(AdminRoutes::PROGRAMMATION_LIST);
 
 
-$app->get('/programmation/edit/:id', function ($id) use ($app) {
-    /** @var Slim $app */
-    $app->render('programmation/new_or_edit.twig',
-        array(
-            'mode'=>'edit',
-            'item'=>Presentation::getById($id),
-        )
-    );
-})->name(AdminRoutes::PROGRAMMATION_EDIT);
-
-$app->get('/programmation/new/', function () use ($app) {
-    /** @var Slim $app */
-    $app->render('programmation/new_or_edit.twig',
-        array(
-            'mode'=>'new',
-            'item' => Presentation::create()
-        )
-    );
-})->name(AdminRoutes::PROGRAMMATION_NEW);
-
-$app->post('/programmation/create', function () use ($app) {
-    /** @var Slim $app */
-    $p = Presentation::create();
-    foreach($_POST as $k=>$v){
-        $p->set($k, $v);
-    }
-    $p->save();
-    $id = $p->get('id');
-
-    $app->flash('programmation', 'Créé!');
-    $app->redirect($app->urlFor(AdminRoutes::PROGRAMMATION_EDIT, array('id'=>$id)));
-})->name(AdminRoutes::PROGRAMMATION_CREATE);
-
-$app->post('/programmation/update/:id', function ($id) use ($app) {
-    /** @var Slim $app */
-    $p = Presentation::getById($id);
-    foreach($_POST as $k=>$v){
-        $p->set($k, $v);
-    }
-    $p->save();
-    $app->flash('programmation', 'Sauvegardé!');
-    $app->redirect($app->urlFor(AdminRoutes::PROGRAMMATION_EDIT, array('id'=>$id)));
-})->name(AdminRoutes::PROGRAMMATION_UPDATE);
-
-$app->get('/programmation/delete/:id', function ($id) use ($app) {
-    /** @var Slim $app */
-    $p = Presentation::getById($id);
-    $p->delete();
-
-    $app->flash('programmation', 'Supprimé!');
-    $app->redirect($app->urlFor(AdminRoutes::PROGRAMMATION_LIST));
-})->name(AdminRoutes::PROGRAMMATION_DELETE);
+include __DIR__."/../includes/programmation_crud.inc.php";
 
 $app->run();
 
---
+++ app/admin/includes/programmation_crud.inc.php
@@ -0,0 +1,54 @@
+<?php
+global $app;
+$app->get('/programmation/edit/:id', function ($id) use ($app) {
+    /** @var Slim $app */
+    $app->render('programmation/new_or_edit.twig',
+        array(
+            'mode'=>'edit',
+            'item'=>Presentation::getById($id),
+        )
+    );
+})->name(AdminRoutes::PROGRAMMATION_EDIT);
+
+$app->get('/programmation/new/', function () use ($app) {
+    /** @var Slim $app */
+    $app->render('programmation/new_or_edit.twig',
+        array(
+            'mode'=>'new',
+            'item' => Presentation::create()
+        )
+    );
+})->name(AdminRoutes::PROGRAMMATION_NEW);
+
+$app->post('/programmation/create', function () use ($app) {
+    /** @var Slim $app */
+    $p = Presentation::create();
+    foreach($_POST as $k=>$v){
+        $p->set($k, $v);
+    }
+    $p->save();
+    $id = $p->get('id');
+
+    $app->flash('programmation', 'Créé!');
+    $app->redirect($app->urlFor(AdminRoutes::PROGRAMMATION_EDIT, array('id'=>$id)));
+})->name(AdminRoutes::PROGRAMMATION_CREATE);
+
+$app->post('/programmation/update/:id', function ($id) use ($app) {
+    /** @var Slim $app */
+    $p = Presentation::getById($id);
+    foreach($_POST as $k=>$v){
+        $p->set($k, $v);
+    }
+    $p->save();
+    $app->flash('programmation', 'Sauvegardé!');
+    $app->redirect($app->urlFor(AdminRoutes::PROGRAMMATION_EDIT, array('id'=>$id)));
+})->name(AdminRoutes::PROGRAMMATION_UPDATE);
+
+$app->get('/programmation/delete/:id', function ($id) use ($app) {
+    /** @var Slim $app */
+    $p = Presentation::getById($id);
+    $p->delete();
+
+    $app->flash('programmation', 'Supprimé!');
+    $app->redirect($app->urlFor(AdminRoutes::PROGRAMMATION_LIST));
+})->name(AdminRoutes::PROGRAMMATION_DELETE);
