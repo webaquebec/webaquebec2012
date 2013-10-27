ff78b66d4185f9433c20cef2e2b4d57a59891f94
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2013-10-27 16:41:29 -0400

    Add heroku app shared conf

--- .profile
+++ .profile
@@ -1,2 +1,3 @@
 #!/bin/sh
 sass app/frontend/http_serve/assets/css/style.sass:app/frontend/http_serve/assets/css/style-12.css
+cp app/shared/conf.{heroku.php,php}
---
+++ app/shared/conf.heroku.php
@@ -0,0 +1,26 @@
+<?php
+
+$config = array(
+    'name' => 'identifiy_name',
+    'auth' => array(
+        'password_salt' => 'hash-hash-hash-hash-hash',
+        'users' => array(
+            /* use domain.com/hash/password-to-hash in order to generate hashed password using the provided salt. */
+            array(
+                'name' => 'user',
+                'hash' => 'hash',
+                'role' => Auth::ROLE_ADMIN,
+            ),
+        ),
+    ),
+    'db' => array(
+        'host' => 'host.domain',
+        'port' => '3306',
+        'user' => 'user',
+        'password' => 'pwd',
+        'database' => 'dbname',
+    ),
+    'ironweb_live' => FALSE,
+    'cache' => TRUE,
+    'debug' => FALSE,
+);
